06393809d4346faf1f70c2d6d979f12570f4f2b1
Rivan Albani Ray <rivanalbani88@gmail.com>
2022-06-15 16:21:04 +0700

    done jobs api

--- 06-jobs-api-prod/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const {login, register} = require('../controllers/auth');
-
-router.post('/register', register).post('/login', login);
-
-module.exports = router;
