457f292f7e405875ba530be2318045cde03d011a
Rivan Albani Ray <rivanalbaniray88@gmail.com>
2022-06-20 15:53:34 +0700

    js

---
+++ fundamental-js/variable.js
@@ -0,0 +1,14 @@
+let lastName = 'Skywalker';
+
+console.log(lastName);
+
+/* output
+Skywalker
+*/
+
+const z = 100;
+console.log(z);
+z = 200;
+console.log(z);
+
+/* TypeError: Assignment to constant variable. */
