86d084f62256a30c69a1de77e3cf669a39bd0d26
Rivan Albani Ray <rivanalbaniray88@gmail.com>
2022-06-20 17:22:09 +0700

    up

---
+++ notes-app-back-end/src/validator/notes/index.js
@@ -0,0 +1,13 @@
+const {notePayloadSchema} = require('./schema');
+const InvariantError = require('../../exceptions/InvariantError');
+
+const NotesValidator = {
+  validateNotePayload: (payload) => {
+    const validationResult = notePayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = NotesValidator;
